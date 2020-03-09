0cd28dcdb525c3d86ecf1b801dac4ffc78b22b00
@kostyaFrom <korustlt@gmail.com>
2020-03-09 22:10:42 +0400

    add getSongs method

--- app/src/main/java/com/bulyginkonstantin/playme/activities/MusicPlayerActivity.kt
+++ app/src/main/java/com/bulyginkonstantin/playme/activities/MusicPlayerActivity.kt
@@ -1,13 +1,103 @@
 package com.bulyginkonstantin.playme.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bulyginkonstantin.playme.R
+import com.bulyginkonstantin.playme.adapters.MusicAdapter
+import com.bulyginkonstantin.playme.data.Music
+import kotlinx.android.synthetic.main.activity_music_player.*
+
+private const val REQUEST_CODE_EXTERNAL_STORAGE = 1
 
 class MusicPlayerActivity : AppCompatActivity() {
+
+    private lateinit var songList: MutableList<Music>
+    private lateinit var adapter: MusicAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
+        songList = mutableListOf()
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions()
+        }
+
+
+    }
+
+    private fun checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getSongs()
+        } else {
+            //false-> user asked not to ask me anymore || permission disabled
+            //true-> reject before want to use the future again
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                Toast.makeText(this, "Music player needs Access to your files", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_CODE_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_EXTERNAL_STORAGE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //read songs
+                getSongs()
+            } else {
+                Toast.makeText(this, "Permission is not granted", Toast.LENGTH_SHORT).show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun getSongs() {
+        val selection = MediaStore.Audio.Media.IS_MUSIC
+        val projection = arrayOf(
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA
+        )
+        val cursor = contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            null
+        )
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                songList.add(Music(cursor.getString(0), cursor.getString(1), cursor.getString(2)))
+            }
+        }
+        cursor?.close()
+        adapter = MusicAdapter(songList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 }
