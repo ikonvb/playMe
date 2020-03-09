0acfa1ed0d943a8235914cc9c2593a012b87c965
@kostyaFrom <korustlt@gmail.com>
2020-03-09 20:33:22 +0400

    design layout for music player

---
+++ app/src/main/java/com/bulyginkonstantin/playme/MusicPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.bulyginkonstantin.playme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
